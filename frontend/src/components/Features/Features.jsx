3a8e01738902d77255c69b96a99276fa0b713114
SaiSumanth <saisumanth2004.bandaru@gmail.com>
2026-07-06 23:56:37 +0530

    feat: initialize features section structure and design

---
+++ frontend/src/components/Features/Features.jsx
@@ -0,0 +1,27 @@
+import "./Features.css";
+import {FaMagic} from "react-icons/fa";
+
+function Features() {
+    return (
+        <section className="features">
+
+            <div className="container">
+
+                <div classNmae="section-header">
+
+                    {/*Bagde*/}
+                    {/*Heading*/}
+                    {/*Description*/}
+
+                </div>
+                <div className="features-grid">
+
+                    {/* Feature Cards */}
+
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default Features;
